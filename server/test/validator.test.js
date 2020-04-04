b01da71ec94f02b764ff52125eee933873f5eac7
Riteish <riteish@Riteishs-iPro.fios-router.home>
2020-04-04 16:24:07 -0400

    more changes and initial test

---
+++ server/test/validator.test.js
@@ -0,0 +1,25 @@
+const validator = require("../src/helpers/validator");
+
+test("should be true if data matches the schema", () => {
+  const mockData = {
+    _id: "1",
+    firstName: "abc",
+    lastName: "xyz",
+    hireDate: "2010-04-12",
+    role: "LACKEY",
+  };
+  const validation = validator(mockData);
+  expect(validation).toBe(true);
+});
+
+test("should be false if data does not match the schema", () => {
+  const mockData = {
+    _id: "1",
+    firstName: "abc",
+    lastName: "",
+    hireDate: "2010-04-12",
+    role: "LACKEY",
+  };
+  const validation = validator(mockData);
+  expect(validation).toBe(false);
+});
